ce230f6e1a76a1f947df0093d2c998d1c72a3e55
Simon Le Serve <simon.leserve@gmail.com>
2024-05-07 08:53:08 +1000

    Added Babel and updated dependencies
    - Created a new .babelrc file with the preset "next/babel"
    - Updated .gitignore to ignore package-lock.json
    - Added "@babel/core" and "react-icons" to the project dependencies in package.json, also rearranged existing ones
    - Refactored layout.js by changing import paths and modifying HTML structure
    - Expanded Header.jsx functionality by adding MenuItem components for navigation and styling elements
    - Created a new component, MenuItem.jsx, for reusable menu items in the header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,21 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "./components/Header";
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Header from "@/components/Header"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "IMDb clone",
   description: "This a is movie database clone"
-};
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Header>
-        <title>IMDb clone</title>
-      </Header>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import MenuItem from "./MenuItem"
+import { AiFillHome, AiFillContacts } from "react-icons/ai"
+import { BsFillInfo, BsFillInfoCircleFill } from "react-icons/bs"
+import Link from 'next/link'
 
-export const Header = () => {
+const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <div className="flex gap-4">
+        <MenuItem title="Home" address="/" Icon={AiFillHome} />
+        <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
+        <MenuItem title="Contact" address="/contact" Icon={AiFillContacts} />
+      </div>
+      <div className="flex gap-1 items-center">        
+        <Link href="/">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDb</span>
+        </Link>
+        <span className="text-xl hidden sm:inline">Clone</span>
+      </div>
+    </div>
   )
 }
+
+export default Header
