2bb8e971cc61048bde1854ef70a087130a47378b
Simon Le Serve <simon.leserve@gmail.com>
2024-05-07 22:33:00 +1000

    Added About page
    A new 'About' page has been added to the application. This page includes a detailed description of the platform, its origins, team composition, and unique approach to movie reviews. The content is wrapped in a React component for seamless integration into the existing app structure.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className='max-w-6xl mx-auto p-10 gap-4 space-y-10'>
+      <h1 className='text-2xl font-medium text-amber-600'>
+        About Page
+      </h1>
+      <p>
+        Welcome to Mad Movies, the quintessential destination where cinema meets chaos, wrapped
+        in the veneer of the most solemn film critique. At Mad Movies, we pride ourselves on our
+        ability to dissect the most profound narratives and cinematic experiences with the gravity
+        they barely deserve. Born from the visionary mind of a former dental hygienist turned
+        film aficionado, our platform is dedicated to the serious business of not taking serious
+        films too seriously.
+      </p>
+      <p>
+        Our team comprises a diverse group of ex-politicians, retired circus performers, and one
+        astrologer, all united by their unbridled passion for films and an equally uncontrolled
+        disdain for conventional reviews. Each review is crafted with a meticulous lack of detail,
+        ensuring that readers are left bewilderingly informed. We dive deep into the art of film
+        critique, using groundbreaking methodologies such as the Emotional Popcorn Index (EPI) and
+        the Random Plot Hole Generator, bringing a new level of rigor to the inherently subjective
+        task of movie evaluation.
+      </p>
+      <p>
+        Mad Movies is more than just a review site—it’s a paradox wrapped in an enigma, broadcast
+        through the medium of cinema critique. Whether you’re looking to justify your irrational
+        affection for bad horror movies or seeking scholarly validation for watching cult classics
+        backwards, Mad Movies is your go-to source. Remember, in the world of Mad Movies, every
+        film is a puzzle, every review a labyrinth, and every reader a delightedly confused participant
+        in the grand scheme of cinematic absurdity. Join us, and embrace the madness.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
